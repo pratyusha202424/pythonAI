dab7e0d4079fb6a735d55038c3df143def38381e
pratyusha d <pratyusha.job31@gmail.com>
2026-01-18 21:46:19 -0800

    updating first commit

---
+++ sample.py
@@ -0,0 +1,23 @@
+print("Hello, World!");
+cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
+for city in cities:
+    print(city);
+
+planets ={"mercury": 1, "venus": 2, "earth": 3, "mars": 4};
+for planet in planets:
+ # print(planet, planets[planet]);
+ print(planets[planet]);
+
+
+
+names=("Alice", "Bob", "Charlie", "David", "Eve");
+for name in names: 
+    print(name);
+
+
+print(cities[2]);
+print(planets["earth"]);
+print(names[0]);
+
+
+
